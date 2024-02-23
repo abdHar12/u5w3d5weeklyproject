bae2ca90d1459099b002c3bfe981b06df0a3ab62
abdHar12 <145681190+abdHar12@users.noreply.github.com>
2024-02-23 11:09:00 +0100

    Initial commit

---
+++ src/main/java/harouane/u5w3d5weeklyproject/U5w3d5weeklyprojectApplication.java
@@ -0,0 +1,13 @@
+package harouane.u5w3d5weeklyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w3d5weeklyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w3d5weeklyprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/harouane/u5w3d5weeklyproject/U5w3d5weeklyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package harouane.u5w3d5weeklyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w3d5weeklyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
